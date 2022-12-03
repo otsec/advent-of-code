eb2bcf4475b0c226f2e3651121317c49d43aa7b0
Artem Belov <otsec.ru@gmail.com>
2022-12-03 09:40:22 +0100

    Day 2 completed

---
+++ 2022/day01/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/otsec/advent-of-code/util"
+)
+
+func main() {
+	var part int
+	flag.IntVar(&part, "part", 1, "part 1 or 2")
+	flag.Parse()
+	fmt.Println("Running part", part)
+
+	if part == 1 {
+		ans := part1(util.ReadFile("./input.txt"))
+		util.CopyToClipboard(fmt.Sprintf("%v", ans))
+		fmt.Println("Output:", ans)
+	} else {
+		ans := part2(util.ReadFile("./input.txt"))
+		util.CopyToClipboard(fmt.Sprintf("%v", ans))
+		fmt.Println("Output:", ans)
+	}
+}
+
+func part1(input string) int {
+	lines := strings.Split(input, "\n")
+
+	max := 0
+	curr := 0
+	for _, item := range lines {
+		if item == "" {
+			if curr > max {
+				max = curr
+			}
+			curr = 0
+		} else {
+			if cal, err := strconv.Atoi(item); err == nil {
+				curr += cal
+			}
+		}
+	}
+
+	return max
+}
+
+func part2(input string) int {
+	lines := strings.Split(input, "\n")
+
+	elves := []int{}
+	curr := 0
+	for _, item := range lines {
+		if item == "" {
+			elves = append(elves, curr)
+			curr = 0
+		} else {
+			if cal, err := strconv.Atoi(item); err == nil {
+				curr += cal
+			}
+		}
+	}
+
+	sort.Ints(elves)
+
+	return elves[len(elves)-1] + elves[len(elves)-2] + elves[len(elves)-3]
+}
---
+++ 2022/day02-rock-paper-scissors/main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/otsec/advent-of-code/util"
+	"strings"
+)
+
+func main() {
+	var part int
+	flag.IntVar(&part, "part", 1, "part 1 or 2")
+	flag.Parse()
+	fmt.Println("Running part", part)
+
+	if part == 1 {
+		ans := part1(util.ReadFile("./input.txt"))
+		util.CopyToClipboard(fmt.Sprintf("%v", ans))
+		fmt.Println("Output:", ans)
+	} else {
+		ans := part2(util.ReadFile("./input.txt"))
+		util.CopyToClipboard(fmt.Sprintf("%v", ans))
+		fmt.Println("Output:", ans)
+	}
+}
+
+func part1(input string) int {
+	lines := strings.Split(input, "\n")
+
+	total := 0
+	for _, line := range lines {
+		items := strings.Split(line, " ")
+		var1, var2 := items[0], items[1]
+		total += playForWin(var1, var2)
+	}
+
+	return total
+}
+
+func part2(input string) int {
+	lines := strings.Split(input, "\n")
+
+	total := 0
+	for _, line := range lines {
+		items := strings.Split(line, " ")
+		var1, var2 := items[0], items[1]
+		total += playSecretStrategy(var1, var2)
+	}
+
+	return total
+}
+
+type GameMapping struct {
+	score int
+	wins  string
+	draws string
+	loses string
+}
+
+func playForWin(var1, var2 string) int {
+	result := 0
+
+	mapping := map[string]GameMapping{
+		"X": {score: 1, wins: "C", draws: "A"},
+		"Y": {score: 2, wins: "A", draws: "B"},
+		"Z": {score: 3, wins: "B", draws: "C"},
+	}
+
+	this := mapping[var2]
+
+	result += this.score
+	if var1 == this.wins {
+		result += 6
+	}
+	if var1 == this.draws {
+		result += 3
+	}
+
+	return result
+}
+
+func playSecretStrategy(var1, var2 string) int {
+	result := 0
+
+	scores := map[string]int{
+		"A": 1, // rock
+		"B": 2, // paper
+		"C": 3, // scissors
+	}
+	mapping := map[string]GameMapping{
+		"A": {wins: "C", draws: "A", loses: "B"},
+		"B": {wins: "A", draws: "B", loses: "C"},
+		"C": {wins: "B", draws: "C", loses: "A"},
+	}
+
+	if var2 == "X" {
+		myChoice := mapping[var1].wins
+		result += scores[myChoice]
+	}
+	if var2 == "Y" {
+		myChoice := mapping[var1].draws
+		result += scores[myChoice] + 3
+	}
+	if var2 == "Z" {
+		myChoice := mapping[var1].loses
+		result += scores[myChoice] + 6
+	}
+
+	return result
+}
