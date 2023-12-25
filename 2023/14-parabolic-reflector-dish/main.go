40bb1ad34f3ffe9a804dd16e519cd6a48adb6c2c
Artem Belov <otsec.ru@gmail.com>
2023-12-25 15:19:16 +0100

    Year 2023 Day 14 part 1 completed.

---
+++ 2023/14-parabolic-reflector-dish/main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	_ "embed"
+	"flag"
+	"fmt"
+	"github.com/alexchao26/advent-of-code-go/util"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+func init() {
+	// do this in init (not main) so test file has same input
+	input = strings.TrimRight(input, "\n")
+	if len(input) == 0 {
+		panic("empty input.txt file")
+	}
+}
+
+func main() {
+	var part int
+	flag.IntVar(&part, "part", 1, "part 1 or 2")
+	flag.Parse()
+	fmt.Println("Running part", part)
+
+	if part == 1 {
+		ans := part1(input)
+		util.CopyToClipboard(fmt.Sprintf("%v", ans))
+		fmt.Println("Output:", ans)
+	} else {
+		ans := part2(input)
+		util.CopyToClipboard(fmt.Sprintf("%v", ans))
+		fmt.Println("Output:", ans)
+	}
+}
+
+func part1(input string) (ans int) {
+	field := NewCharField(input)
+
+	for x := 0; x < field.Width(); x++ {
+		slideToY := 0
+		mirrors := 0
+		for y := 0; y < field.Height(); y++ {
+			c := Coord{x, y}
+
+			isMirror := field.At(c) == 'O'
+			isRock := field.At(c) == '#'
+			isLast := y == field.Height()-1
+
+			if isMirror {
+				mirrors++
+			}
+
+			if isRock || isLast {
+				for i := 0; i < mirrors; i++ {
+					ans += field.Height() - slideToY - i
+				}
+
+				mirrors = 0
+				slideToY = y + 1
+			}
+		}
+	}
+
+	return ans
+}
+
+func part2(input string) (ans int) {
+	field := NewCharField(input)
+	_ = field
+
+	return ans
+}
+
+type Coord struct {
+	x, y int
+}
+
+type CharField struct {
+	lines []string
+}
+
+func (f *CharField) Width() int {
+	return len(f.lines[0])
+}
+
+func (f *CharField) Height() int {
+	return len(f.lines)
+}
+
+func (f *CharField) Within(c Coord) bool {
+	if c.y < 0 || c.y >= f.Height() {
+		return false
+	}
+	if c.x < 0 || c.x >= f.Width() {
+		return false
+	}
+	return true
+}
+
+func (f *CharField) At(c Coord) byte {
+	return f.lines[c.y][c.x]
+}
+
+func (f *CharField) Set(c Coord, v byte) {
+	line := []byte(f.lines[c.y])
+	line[c.x] = v
+	f.lines[c.y] = string(line)
+}
+
+func (f *CharField) ToString() string {
+	return strings.Join(f.lines, "\n")
+}
+
+func (f *CharField) Print() {
+	fmt.Println(f.ToString())
+}
+
+func NewCharField(input string) *CharField {
+	lines := strings.Split(input, "\n")
+	return &CharField{lines}
+}
