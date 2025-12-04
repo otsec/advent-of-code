0e4cf17c1dc5306b9b1070e5d2707b665fd54af2
Artem Belov <otsec.dev@gmail.com>
2025-12-04 09:33:47 +0100

    Year 2025 Day 4 completed.

---
+++ 2025/4-printing-department/main.go
@@ -0,0 +1,209 @@
+package main
+
+import (
+	"bytes"
+	_ "embed"
+	"flag"
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+func init() {
+	// do this in init (not main) so test file has same input
+	input = strings.TrimRight(input, "\n")
+	if len(input) == 0 {
+		panic("empty input.txt file")
+	}
+}
+
+func main() {
+	var part int
+	flag.IntVar(&part, "part", 1, "part 1 or 2")
+	flag.Parse()
+	fmt.Println("Running part", part)
+
+	if part == 1 {
+		ans := part1(input)
+		CopyToClipboard(fmt.Sprintf("%v", ans))
+		fmt.Println("Output:", ans)
+	} else {
+		ans := part2(input)
+		CopyToClipboard(fmt.Sprintf("%v", ans))
+		fmt.Println("Output:", ans)
+	}
+}
+
+func part1(input string) (ans int) {
+	field := NewCharField(input)
+
+	for x := 0; x < field.Width(); x++ {
+		for y := 0; y < field.Height(); y++ {
+			c := Coord{x, y}
+			if field.At(c) == '@' && calcAround(field, c) < 4 {
+				ans++
+			}
+		}
+	}
+
+	return ans
+}
+
+func part2(input string) (ans int) {
+	field := NewCharField(input)
+
+	for {
+		toRemove := []Coord{}
+
+		for x := 0; x < field.Width(); x++ {
+			for y := 0; y < field.Height(); y++ {
+				c := Coord{x, y}
+				if field.At(c) == '@' && calcAround(field, c) < 4 {
+					toRemove = append(toRemove, c)
+				}
+			}
+		}
+
+		for _, c := range toRemove {
+			field.Set(c, '#')
+		}
+
+		ans += len(toRemove)
+		if len(toRemove) == 0 {
+			break
+		}
+	}
+
+	return ans
+}
+
+func calcAround(field *CharField, c Coord) int {
+	coords := []Coord{
+		c.Top(),
+		c.Top().Right(),
+		c.Right(),
+		c.Bottom().Right(),
+		c.Bottom(),
+		c.Bottom().Left(),
+		c.Left(),
+		c.Top().Left(),
+	}
+
+	ans := 0
+	for _, coord := range coords {
+		if field.Within(coord) && field.At(coord) == '@' {
+			ans += 1
+		}
+	}
+
+	return ans
+}
+
+type Coord struct {
+	x, y int
+}
+
+func (c Coord) Top() Coord {
+	return Coord{c.x, c.y - 1}
+}
+
+func (c Coord) Right() Coord {
+	return Coord{c.x + 1, c.y}
+}
+
+func (c Coord) Bottom() Coord {
+	return Coord{c.x, c.y + 1}
+}
+
+func (c Coord) Left() Coord {
+	return Coord{c.x - 1, c.y}
+}
+
+type Direction int
+
+const (
+	Top Direction = iota
+	Right
+	Bottom
+	Left
+)
+
+func (c Coord) Next(dir Direction) Coord {
+	switch dir {
+	case Top:
+		return Coord{c.x, c.y - 1}
+	case Right:
+		return Coord{c.x + 1, c.y}
+	case Bottom:
+		return Coord{c.x, c.y + 1}
+	case Left:
+		return Coord{c.x - 1, c.y}
+	default:
+		panic("unknown direction type")
+	}
+}
+
+type CharField struct {
+	lines []string
+}
+
+func (f *CharField) Width() int {
+	return len(f.lines[0])
+}
+
+func (f *CharField) Height() int {
+	return len(f.lines)
+}
+
+func (f *CharField) Within(c Coord) bool {
+	if c.y < 0 || c.y >= f.Height() {
+		return false
+	}
+	if c.x < 0 || c.x >= f.Width() {
+		return false
+	}
+	return true
+}
+
+func (f *CharField) At(c Coord) byte {
+	return f.lines[c.y][c.x]
+}
+
+func (f *CharField) Set(c Coord, v byte) {
+	line := []byte(f.lines[c.y])
+	line[c.x] = v
+	f.lines[c.y] = string(line)
+}
+
+func (f *CharField) ToString() string {
+	return strings.Join(f.lines, "\n")
+}
+
+func (f *CharField) Print() {
+	fmt.Println(f.ToString())
+}
+
+func NewCharField(input string) *CharField {
+	lines := strings.Split(input, "\n")
+	return &CharField{lines}
+}
+
+// CopyToClipboard is for macOS
+func CopyToClipboard(text string) error {
+	command := exec.Command("pbcopy")
+	command.Stdin = bytes.NewReader([]byte(text))
+
+	if err := command.Start(); err != nil {
+		return fmt.Errorf("error starting pbcopy command: %w", err)
+	}
+
+	err := command.Wait()
+	if err != nil {
+		return fmt.Errorf("error running pbcopy %w", err)
+	}
+
+	return nil
+}
